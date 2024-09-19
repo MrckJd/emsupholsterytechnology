29dfe13518dd856b20e8d5a704ea08f1a637532f
marckjude <dmarckjude25@gmail.com>
2024-09-19 19:52:08 +0800

    Choose Design Images

--- script/indexScript.js
+++ script/indexScript.js
@@ -1,8 +1,9 @@
 function onScrollEvent(){
     const navbar = document.getElementById("navbar");
     if (window.scrollY > 50) {
-        navbar.classList.remove("bg-opacity-20");
+        navbar.classList.add("bg-opacity-80");
     } else {
         navbar.classList.add("bg-opacity-20");
+        navbar.classList.remove("bg-opacity-80")
     }
 }
