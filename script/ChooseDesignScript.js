d044f3f429928764cb6829a0fbc03ff841f1781b
dmarc <dmarc@DESKTOP-2NK0V4M>
2024-09-23 00:19:18 +0800

    extracted data in design choosing in all makers in all years, and partially extracted all models in 2025 and 2024 in all models in the makers years

--- script/ChooseDesignScript.js
+++ script/ChooseDesignScript.js
@@ -1,28 +1,50 @@
 function onSelectedYear(){
-    const selectedYear = document.getElementById("years");
-    if(selectedYear.value=="2021"){
-        document.getElementById("makersDiv").classList.remove("hidden");
-    }else{
-        document.getElementById("makersDiv").classList.add("hidden");
-    }
+    const selectedYear = document.getElementById("years").value;
+    const modelsDiv = document.getElementById("modelsDiv");
+    const makersDiv = document.getElementById("makersDiv");
+    const modelsSelect = document.getElementById("models");
+    const makersSelect = document.getElementById("makers");
+
+    modelsSelect.innerHTML = '<option value="">Select Make</option>';
+    makersSelect.innerHTML = '<option value="">Select Make</option>';
+    if (selectedYear && MakeByYear[selectedYear]) {
+        const makes = MakeByYear[selectedYear];
+        makersDiv.classList.remove('hidden');
+  
+        makes.forEach(make => {
+          const option = document.createElement("option");
+          option.value = make;
+          option.text = make;
+          makersSelect.appendChild(option);
+          console.log(makes);
+        });
+      } else {
+        makersDiv.classList.add('hidden');
+        modelsDiv.classList.add('hidden');
+      }
 }
 
-function onSelectedMake(maker){
-    const selectedMake = document.getElementById("makers");
-    switch(selectedMake.value){
-        case "BUICK":
-            document.getElementById(maker).classList.remove("hidden");
-            break;
-        case "CADILLAC":
-            selectedModel = document.getElementById('CadillacModelsDiv');
-            break;
-        case "CHEVROLET":
-            selectedModel = document.getElementById('ChevroletModelsDiv');
-            break;
-        default: 
-            console.log('test');
+function onSelectedMake(){
+    const selectedYear = document.getElementById("years").value;
+    const makersSelect = document.getElementById("makers").value;
+    const modelsDiv = document.getElementById("ModelsDiv");
+    const modelsSelect = document.getElementById("models");
 
-    }
+    modelsSelect.innerHTML = '<option value="">Select Make</option>';
+    if (selectedYear && makersSelect && MakeModel[selectedYear][makersSelect]) {
+        const model = MakeModel[selectedYear][makersSelect];
+        console.log(model);
+        modelsDiv.classList.remove('hidden');
+  
+        model.forEach(model => {
+          const option = document.createElement("option");
+          option.value = model;
+          option.text = model;
+          modelsSelect.appendChild(option);
+        });
+      } else {
+        modelsDivDiv.classList.add('hidden');
+      }
 }
 
 function onSelectedChevroletModels(){
--- script/indexScript.js
+++ script/indexScript.js
@@ -1,9 +1,14 @@
 function onScrollEvent(){
     const navbar = document.getElementById("navbar");
+    const logo = document.getElementById("logo");
     if (window.scrollY > 50) {
-        navbar.classList.add("bg-opacity-80");
+        navbar.classList.add("bg-opacity-90");
+        logo.classList.remove("my-6","md:h-24");
+        logo.classList.add("my-2","md:h-14");
     } else {
-        navbar.classList.add("bg-opacity-20");
-        navbar.classList.remove("bg-opacity-80")
+        navbar.classList.add("bg-opacity-40");
+        navbar.classList.remove("bg-opacity-90");
+        logo.classList.remove("my-2", "md:h-14");
+        logo.classList.add("my-6","md:h-24");
     }
 }
